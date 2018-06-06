17819a6dbd7e56ea12565c9b9575faf59482edd4
tsandi <naugh_t_j@hotmail.com>
2018-06-06 18:32:26 +0200

    Erster Push.

---
+++ JavaScript/bootstrap.js
@@ -0,0 +1,20 @@
+// Code goes here
+
+$(function() {
+    var $previewContainer = $('#comment-md-preview-container');
+    $previewContainer.hide();
+    var $md = $("#comment-md").markdown({
+        autofocus: false,
+        height: 270,
+        iconlibrary: 'fa',
+        onShow: function(e) {
+            e.hideButtons('cmdPreview');
+            //e.change(e);
+        }
+        /*onChange: function(e) {
+            var content = e.parseContent();
+            if (content === '') $previewContainer.hide();
+            else $previewContainer.show().find('#comment-md-preview').html(content).find('table').addClass('table table-bordered table-striped table-hover');
+        }*/
+    });
+});
