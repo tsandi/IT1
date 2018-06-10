c83023d43e7f4078d760d0a6411afaa159d02802
Ali <aliabdin95@gmx.de>
2018-06-10 17:26:28 +0200

    Info Box wurde in die Startseite eingefügt, CSS geändert

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
     <script type='text/javascript' src="JavaScript/bootstrap.js"></script>
     <script type='text/javascript' src="JavaScript/getBlogsForStartPage.js"></script>
 
+
 </head>
 
 <body>
