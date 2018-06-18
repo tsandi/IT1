7185aa8ea511bf333b41b697b45e97135136ba11
Ali <aliabdin95@gmx.de>
2018-06-18 19:31:28 +0200

    Merge branch 'TejansChanges' of https://github.com/tsandi/IT1
    # Conflicts:
    #	DataBaseJSON/BlogDataBase.JSON
    #	displayBlog.php
    #	startPage.html
    #	uploadNewPicture.php

--- commentForBlog.php
+++ commentForBlog.php
@@ -9,13 +9,14 @@ if(isset($_POST['post'])){
     $name = $_POST["name"];
     $email = $_POST["email"];
     $mes = $_POST["mes"];
+    $blogId = $_GET['page'];
 
-$write = fopen("com.txt", "a+");
+$write = fopen("$blogId.txt", "a+");
 $date = date("Y-m-d");
 fwrite($write, "Name: <b><u>$name</u><b><u> Datum: $date</u></b><br><b><u>E-Mail: $email</u></b><br>Kommentar: $mes<br><br>");
 fclose($write);
 
- $read = fopen("com.txt", "r+t");
+ $read = fopen("$blogId.txt", "r+t");
  echo "All comments:";
     echo "<br> ";
  while(!feof($read)){
@@ -26,7 +27,7 @@ fclose($write);
 
     else {
 
-    $read = fopen("com.txt", "r+t");
+    $read = fopen("$blogId.txt", "r+t");
     echo "All comments:";
         echo "<br> ";
      while(!feof($read)){
